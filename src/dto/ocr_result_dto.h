93728ba0bdfe39738e5fbc1647ba3e05fdef0069
chenyuanqin <chen_yu@worksap.co.jp>
2015-12-02 16:28:44 +0800

    nothing

--- src/dto/ocr_result_dto.h
+++ src/dto/ocr_result_dto.h
@@ -1,5 +1,5 @@
 /*
- * OcrResultDto.h
+ * ocr_result_dto.h
  *
  *  Created on: Dec 1, 2015
  *      Author: michael
@@ -7,34 +7,39 @@
 
 #ifndef SRC_DTO_OCR_RESULT_DTO_H_
 #define SRC_DTO_OCR_RESULT_DTO_H_
-
 #include <string>
 #include <vector>
 
-
+#include "../util/string_util.h"
+using namespace std;
+using namespace cv;
 class ResultUnit {
 public:
-	std::vector<cv::Point2i> boundingBox;  // every symbol's boundingBox
-	std::string content;                   // symbol's content
-	std::vector<std::pair<std::string,float> > candidates; // other choices for this symbol (content, possibility)
-	float confidence;
-    int lineIndex;
+	vector<cv::Point2i> bounding_box;  // every symbol's bounding_box
+	string content;                   // symbol's content
+	vector<pair<string,float> > candidates; // other choices for this symbol (content, possibility)
+    float confidence;
+	int line_index;
 	ResultUnit()
 	{
-		lineIndex = 0;
+		line_index = 0;
+		confidence = 0;
 	}
-	ResultUnit(std::vector<cv::Point2i> &bbox, std::string content)
+	ResultUnit(vector<Point2i> &bbox, string content)
 	{
-    	this->boundingBox = bbox;
+    	this->bounding_box = bbox;
     	this->content = content;
-    	this->lineIndex = 0;
+    	this->line_index = 0;
+    	this->confidence = 0;
 	}
 };
 class OcrDetailResult {
 private:
-    std::vector<ResultUnit> symbols;
+    vector<ResultUnit> symbols;
 public:
-    void setResult(std::vector<ResultUnit> &symbols)
+    const static int SYMBOL;
+    const static int TEXT;
+    void setResult(vector<ResultUnit> &symbols)
     {
     	this->symbols = symbols;
     }
@@ -42,7 +47,7 @@ public:
     {
     	symbols.clear();
     }
-    void push_back_symbol(std::vector<ResultUnit> &a)
+    void push_back_symbol(vector<ResultUnit> &a)
     {
     	for(int i = 0; i < a.size();i++)
         	symbols.push_back(a[i]);
@@ -51,22 +56,42 @@ public:
     {
     	symbols.push_back(a);
     }
-    std::vector<ResultUnit> getResult() const
+    vector<ResultUnit> getResult()
 	{
     	return symbols;
 	}
-    std::string toString()
+    string toString(int cut_level)
     {
-    	return "";
+    	string res;
+    	switch(cut_level)
+    	{
+    		case 0:
+    			for (int i = 0; i < symbols.size(); i++)
+    			{
+    				res += "("+StringUtil::toString(symbols[i].line_index)+" "+StringUtil::toString(i)+") ";
+    				res += 	symbols[i].content + " " + StringUtil::toString(symbols[i].confidence) + " ";
+    				vector<pair<string,float> > candidates = symbols[i].candidates;
+    				for (int j = 0; j < candidates.size(); j++)
+    				{
+    					res += candidates[j].first + " " + StringUtil::toString(candidates[j].second) + " ";
+    				}
+    				res += "\n";
+    			}
+    			break;
+    		case 1:
+    			break;
+    	}
+    	return res;
     }
 };
+
 //compare as x
 class CompX
 {
 public:
     bool operator ()(const ResultUnit& a, const ResultUnit& b)
     {
-      return a.boundingBox[0].x < b.boundingBox[0].x;
+      return a.bounding_box[0].x < b.bounding_box[0].x;
     }
 };
 // compare as y
@@ -75,7 +100,7 @@ class CompY
 public:
     bool operator ()(const ResultUnit& a, const ResultUnit& b)
     {
-      return a.boundingBox[0].y < b.boundingBox[0].y;
+      return a.bounding_box[0].y < b.bounding_box[0].y;
     }
 };
 

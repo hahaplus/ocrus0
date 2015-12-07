ae241292ce03225b43f0870d83bd0cbae5c22d21
Chang Sun <sun_ch@worksap.co.jp>
2015-12-07 08:41:49 +0800

    Add setup.py to install Python scripts

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+'''
+Provide for dealing with all kinds of OCR result formats
+
+Copyright (C) 2015 Works Applications, all rights reserved
+
+Created on Dec 6, 2015
+
+@author: Chang Sun
+'''
+
+from distutils.core import setup
+
+setup(name='OCRus',
+      version='1.0',
+      description='OCRus OCR CORE',
+      author='Chang Sun',
+      author_email='sun_ch@worksap.co.jp',
+      packages=['ocrus'],
+      )
